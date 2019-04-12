312afb22438adce8de19ad75ea2d37726b4ed34e
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-04-13 00:31:55 +0100

    implement nodemailer to send mails after user sign up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import passport from 'passport';
 import User from '../models/user.model';
+import nodemailer from 'nodemailer';
+import { getMaxListeners } from 'cluster';
 
 const userAuthController = {
     registerUser(req, res){
@@ -7,10 +9,34 @@ const userAuthController = {
         User.register(newUser, req.body.password, (err, user) => {
             if(err){
                 console.log(err);
+            } else {
+                passport.authenticate('local')(req, res, () => {
+                    console.log(`Sucessfully signed up ! Nice to meet you ${req.body.firstName }`);
+                });
+                
+                //SEND MAIL
+
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'adetomiwaadesanya01@gmail.com',
+                        pass: 'bluwaters'
+                    }
+                });
+                var mailOptions = {
+                    from: 'adetomiwaadesanya01@gmail.com',
+                    to: 'phillipowolana@gmail.com',
+                    subject: 'Sign Up Confirmation Rector',
+                    text: 'Thank you for signing up to Rector'
+                };
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if(error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
             }
-            passport.authenticate('local')(req, res, () => {
-                console.log(`Sucessfully signed up ! Nice to meet you ${req.body.firstName }`);
-            });
         });
     },
     loginUser(req, res){
