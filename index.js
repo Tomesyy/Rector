b22c83f1fd4acb6f06aeda0e16aa63f04188e881
bhosun <bhorsun@yahoo.com>
2019-04-13 21:05:09 -0800

    Uploading files for the view and template

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import passport from 'passport';
 import localStrategy from 'passport-local';
 import User from './models/user.model';
 import userProfile from './models/userProfile.model';
+import path from 'path';
 
 // routes
 import userProfileRoutes from './routes/userProfile.route';
@@ -18,6 +19,8 @@ const PORT = 2345;
 mongoose.connect('mongodb://localhost:27017/Rector', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/stylesheets"));
 
 // Passport configuration
 //express-session
@@ -35,6 +38,9 @@ passport.deserializeUser(User.deserializeUser());
 
 
 // routes USAGE
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+"/views/index.html"));
+});
 app.use('/', userProfileRoutes);
 app.use('/', authRoutes);
 
