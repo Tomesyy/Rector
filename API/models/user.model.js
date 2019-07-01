7b06d78d6ebd3c4d07bb3e8535b09073e6b32407
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-07-01 20:50:25 +0100

    adjusted codebase

--- API/models/user.model.js
+++ API/models/user.model.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose';
 import passportLocalMongoose from 'passport-local-mongoose';
+// import passportEmail from 'passport-email';
 
 const UserSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     username: String,
-    emailAddress: String,
+    email: String,
     password: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
+// UserSchema.plugin(passportEmail);
 
 const UserSchemaModel = mongoose.model('User', UserSchema);
 
---
+++ README.md
@@ -0,0 +1,8 @@
+## INSTALLATION GUIDE
+- fork this project
+-clone to your local machine
+- run "npm install"
+- run "npm run dev" to start the server
+- access on "http://localhost:2345"
+
+## PEACE
